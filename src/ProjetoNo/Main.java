686a0715c7922345aecaca37d1313c77561a8b52
thiagoviks <thiagosudgomes@gmail.com>
2021-07-27 20:59:37 -0300

    Refatorado No

--- src/ProjetoNo/Main.java
+++ src/ProjetoNo/Main.java
@@ -2,14 +2,14 @@ package ProjetoNo;
 
 public class Main {
     public static void main(String[] args) {
-        No no1 = new No("Conteúdo no1");
-        No no2 = new No("Conteúdo no2");
+        No<String> no1 = new No<>("Conteúdo no1");
+        No<String> no2 = new No<>("Conteúdo no2");
         no1.setProximoNo(no2);
 
-        No no3 = new No("Conteúdo no3");
+        No<String> no3 = new No<>("Conteúdo no3");
         no2.setProximoNo(no3);
 
-        No no4 = new No("Conteúdo no4");
+        No<String> no4 = new No<>("Conteúdo no4");
         no3.setProximoNo(no4);
         //no1->no2->no3->no4
 
